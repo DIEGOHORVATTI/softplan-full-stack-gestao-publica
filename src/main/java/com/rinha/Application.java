701bb08ff129a981dfb2083efc83213c344734bb
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-11-18 04:55:32 -0300

    Refactor package structure and add custom exception handling with DTOs

--- src/main/java/com/rinha/Application.java
+++ src/main/java/com/rinha/Application.java
@@ -1,4 +1,4 @@
-package com.rinha.backend;
+package com.rinha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/rinha/errors/ControllersExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.rinha.errors;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllersExceptionHandler {
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<ExceptionDTO> threat404() {
+    return ResponseEntity.notFound().build();
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<ExceptionDTO> threatBadRequest(BadRequestException exception) {
+    ExceptionDTO response = new ExceptionDTO(exception.getMessage(), 400);
+    return ResponseEntity.badRequest().body(response);
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<ExceptionDTO> threatInternalServerError() {
+    ExceptionDTO response = new ExceptionDTO("Internal Server Error", 500);
+    return ResponseEntity.internalServerError().body(response);
+  }
+}
